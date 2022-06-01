4659e6196982ed53ce90fc8ebbcaad6065cd57b1
lymine1996 <lymine1996@163.com>
2022-06-01 14:00:14 -0500

    Switch the database connections from local to remote on Heroku

--- db.js
+++ db.js
@@ -6,27 +6,27 @@ const { Client } = require("pg");
 
 // For development on local server
 
-var db = new Client({
-  user: "postgres",
-  password: "password",
-  database: "secas",
-  port: 5432,
-  host: "localhost"
-});
+// var db = new Client({
+//   user: "postgres",
+//   password: "password",
+//   database: "secas",
+//   port: 5432,
+//   host: "localhost"
+// });
 
 // For production on Heroku
 
 // Need to specify ssl attribute
-// var db = new Client({
-//   // Need to set these config variables beforehand in Heroku environment settings
-//   // For more details, refer to https://devcenter.heroku.com/articles/config-vars
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: "d5l5a3g2eqfbbh",
-//   port: 5432,
-//   host: "ec2-100-25-121-253.compute-1.amazonaws.com",
-//   ssl: { rejectUnauthorized: false }
-// }); 
+var db = new Client({
+  // Need to set these config variables beforehand in Heroku environment settings
+  // For more details, refer to https://devcenter.heroku.com/articles/config-vars
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: "d357fss3k147t7",
+  port: 5432,
+  host: "ec2-54-145-224-156.compute-1.amazonaws.com",
+  ssl: { rejectUnauthorized: false }
+}); 
 
 db.connect();
 
--- db_user.js
+++ db_user.js
@@ -2,27 +2,27 @@ const { Client } = require("pg");
 
 // For development on local server
 
-var db_user = new Client({
-  user: "postgres",
-  password: "password",
-  database: "user",
-  port: 5432,
-  host: "localhost"
-});
+// var db_user = new Client({
+//   user: "postgres",
+//   password: "password",
+//   database: "user",
+//   port: 5432,
+//   host: "localhost"
+// });
 
 // For production on Heroku
 
 // Need to specify ssl attribute
-// var db_user = new Client({
-//   // Need to set these config variables beforehand in Heroku environment settings
-//   // For more details, refer to https://devcenter.heroku.com/articles/config-vars
-//   user: process.env.USERDB_USER,
-//   password: process.env.USERDB_PASSWORD,
-//   database: "del8fb189cm86u",
-//   port: 5432,
-//   host: "ec2-3-227-15-75.compute-1.amazonaws.com",
-//   ssl: { rejectUnauthorized: false }
-// }); 
+var db_user = new Client({
+  // Need to set these config variables beforehand in Heroku environment settings
+  // For more details, refer to https://devcenter.heroku.com/articles/config-vars
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: "d357fss3k147t7",
+  port: 5432,
+  host: "ec2-54-145-224-156.compute-1.amazonaws.com",
+  ssl: { rejectUnauthorized: false }
+}); 
 
 db_user.connect();
 
