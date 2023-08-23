a18cbd179b98d539878a8d37788f85ff7275e3e8
lymine1996 <lymine1996@163.com>
2023-08-23 11:00:50 -0500

    Switch database connection for spatial data and add endpoints

--- db.js
+++ db.js
@@ -17,16 +17,21 @@ const { Client } = require("pg");
 // For production on Heroku
 
 // Need to specify ssl attribute
-var db = new Client({
-  // Need to set these config variables beforehand in Heroku environment settings
-  // For more details, refer to https://devcenter.heroku.com/articles/config-vars
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: "d357fss3k147t7",
-  port: 5432,
-  host: "ec2-54-145-224-156.compute-1.amazonaws.com",
+// var db = new Client({
+//   // Need to set these config variables beforehand in Heroku environment settings
+//   // For more details, refer to https://devcenter.heroku.com/articles/config-vars
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: "d357fss3k147t7",
+//   port: 5432,
+//   host: "ec2-54-145-224-156.compute-1.amazonaws.com",
+//   ssl: { rejectUnauthorized: false }
+// }); 
+
+let db = new Client({
+  connectionString: process.env.HEROKU_POSTGRESQL_SILVER_URL,
   ssl: { rejectUnauthorized: false }
-}); 
+});
 
 db.connect();
 
